995609deab964932e390ae6591af34cc51b31f67
KariannaPaola <kariannaarriaga07@gmail.com>
2025-09-29 20:25:07 -0700

    proyecto react

---
+++ React2/mi-primera-app/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BuscadorClima, ClimaActual} from './pages/home';
+import { BtnDark } from './components/BtnDark';
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BuscadorClima/>
+    <ClimaActual/>
+    <BtnDark/>
+  </StrictMode>
+)
+
+
